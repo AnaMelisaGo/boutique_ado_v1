de325b0cd52231a42ed7e8f81b62db9fb178108d
AnaMelisaGo <melisago.rockn@gmail.com>
2022-07-11 19:35:05 +0000

    Customize admin

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
 from .models import Product, Category
 
-admin.site.register(Product)
-admin.site.register(Category)
+
+class ProductAdmin(admin.ModelAdmin):
+    """ To customize display list in admin """
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    """ To customize display list in admin """
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 class Category(models.Model):
     """ Category class """
+
+    class Meta:
+        """ Change name when model name is plural """
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
