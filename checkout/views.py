ac5d376186bc1dcc9b93a7b2753ebdf3becb5aa5
AnaMelisaGo <melisago.rockn@gmail.com>
2022-08-15 04:20:07 +0000

    Add checkout form data caching in payment intent

--- checkout/views.py
+++ checkout/views.py
@@ -1,13 +1,33 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
 import stripe
+import json
 
 from products.models import Product
 from bag.contexts import bag_contents
 from .forms import OrderForm
 from .models import Order, OrderLineItem
+
+
+@require_POST
+def cache_checkout_data(request):
+    """ get the payment intent ID """
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
@@ -60,9 +80,7 @@ def checkout(request):
                     order.delete()
                     return redirect(reverse('view_bag'))
             request.session['save_info'] = 'save-info' in request.POST
-            return redirect(reverse(
-                'checkout_success', args=[order.order_number]
-            ))
+            return redirect(reverse('checkout_success', args=[order.order_number]))
         else:
             messages.error(request, 'There was an error with your form. \
                 Please doble check your information.')
@@ -80,7 +98,7 @@ def checkout(request):
             amount=stripe_total,
             currency=settings.STRIPE_CURRENCY,
         )
-        
+
         order_form = OrderForm()
 
     if not stripe_public_key:
--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -9,20 +9,20 @@ class StripeWH_Handler:
     def handle_event(self, event):
         """ handle a generic/unicorn/unexpected webhook event """
         return HttpResponse(
-            content=f'Webhook received: {event("type")}',
+            content=f'Unhandled webhook received: {event["type"]}',
             status=200
         )
 
-    def handle_payment_succeeded(self, event):
+    def handle_payment_intent_succeeded(self, event):
         """ handle the payment_intent.succeeded webhook from stripe """
+        intent = event.data.object
+        print(intent)
         return HttpResponse(
-            content=f'Webhook received: {event("type")}',
-            status=200
-        )
+            content=f'Webhook received: {event["type"]}',
+            status=200)
 
     def handle_payment_intent_payment_failed(self, event):
         """ handle the payment_intent.payment_failed webhook from stripe """
         return HttpResponse(
-            content=f'Payment Failed Webhook received: {event("type")}',
-            status=200
-        )
+            content=f'Payment Failed Webhook received: {event["type"]}',
+            status=200)
