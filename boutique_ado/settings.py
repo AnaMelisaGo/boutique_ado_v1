ef55e69262584abbff9f6743406389234592d468
AnaMelisaGo <melisago.rockn@gmail.com>
2022-10-26 19:48:49 +0000

    Edit settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -224,6 +224,6 @@ else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
-    EMAIL_HOST = 'smpt.gmail.com'
+    EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_PASS')
