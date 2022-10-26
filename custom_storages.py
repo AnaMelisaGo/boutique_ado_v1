ec8f886ab1148b78686129609643b92b5fff8256
AnaMelisaGo <melisago.rockn@gmail.com>
2022-10-26 11:34:02 +0000

    Deployment part 5

--- custom_storages.py
+++ custom_storages.py
@@ -2,11 +2,11 @@ from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
-class StaticStorage(S3BotoStorage):
+class StaticStorage(S3Boto3Storage):
     """ To store static files """
     location = settings.STATICFILES_LOCATION
 
 
-class MediaStorage(S3BotoStorage):
+class MediaStorage(S3Boto3Storage):
     """ To store Media files """
     location = settings.MEDIAFILES_LOCATION
